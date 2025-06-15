bd598f1bdb78f949ef37b870eb890058defbf5b0
Ermek <ermek@live.ru>
2025-06-15 14:50:35 +0300

    add logger for GET request

--- endpoints/production_order/get.py
+++ endpoints/production_order/get.py
@@ -3,5 +3,9 @@ from endpoints.base_endpoint import Endpoint
 
 class GetOrder(Endpoint):
     def new_object(self, headers):
-        self.response =  requests.get(f'{self.host}/ProductionOrder/v1/ProductionOrders/', headers=headers)
+        self.response =  requests.get(
+            f'{self.host}/ProductionOrder/v1/ProductionOrders/',
+            headers=headers
+        )
         self.response_json = self.response.json()
+        self._log_request_response()
